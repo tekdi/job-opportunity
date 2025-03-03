ec148c238cd1939ddd9c6141f64eca40d825ba49
vaibhav_s <vaibhav_s@tekditechnologies.com>
2025-03-03 10:21:50 +0530

    Issue #234545 chore: Environment Prefix changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,17 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-dotenv.config(); // Load environment variables from .env file
+const envPath = path.resolve(__dirname, '../../.env');
+
+// Check if .env file exists
+import * as fs from 'fs';
+if (fs.existsSync(envPath)) {
+  console.log('.env file found!');
+} else {
+  console.log('.env file NOT found!');
+}
+
+// Load .env file
+dotenv.config({ path: envPath });
 
 export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  // port: parseInt(process.env.DB_PORT, 10) || 5432,
-  port: 5433,
-  username: process.env.DATABASE_USER || 'opportunity_test',
-  password: process.env.DATABASE_PASSWORD || 'opportunity_test',
-  database: process.env.DATABASE_NAME || 'opportunities',
-  //entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+  type: process.env.DATABASE_TYPE as any,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '5433', 10),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false,
   logging: process.env.DB_LOGGING === 'true',
